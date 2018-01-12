6eb5c333778876811f13ee74fc4a4017008c1f5d
Sodaguan <578673813@qq.com>
2018-01-12 14:55:52 +0800

    '将代码提交到远程'

---
+++ src/base/silder/silder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot>
+
+      </slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+   min-height:1px
+</style>
